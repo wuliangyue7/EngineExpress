45bc218200a184c26468ae11dd3fc8853dae1190
wuliangyu <=>
2018-08-24 16:58:15 +0800

    init project struct

---
+++ engineexpress-core/src/main/java/com/wuly/engineexpress/core/EngineExpress.java
@@ -0,0 +1,7 @@
+package com.wuly.engineexpress.core;
+
+public class EngineExpress {
+    public void funcTest(){
+        System.out.println("CoreTest funcTest");
+    }
+}
---
+++ engineexpress-example/src/main/java/com/wuly/engineexpress/example/EngineExpressTest.java
@@ -0,0 +1,10 @@
+package com.wuly.engineexpress.example;
+import com.wuly.engineexpress.core.*;
+
+public class EngineExpressTest {
+
+    static public void main(String[] args){
+        EngineExpress engineExpress = new EngineExpress();
+        engineExpress.funcTest();
+    }
+}
