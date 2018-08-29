8249695d87e4dfe86e7b6d70255234e630176b73
wuliangyu <=>
2018-08-29 18:27:26 +0800

    添加一些依赖库

--- engineexpress-example/src/main/java/com/wuly/engineexpress/example/EngineExpressTest.java
+++ engineexpress-example/src/main/java/com/wuly/engineexpress/example/EngineExpressTest.java
@@ -1,10 +1,21 @@
 package com.wuly.engineexpress.example;
 import com.wuly.engineexpress.core.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EngineExpressTest {
 
+    private static Logger logger = LoggerFactory.getLogger(EngineExpressTest.class);
+
     static public void main(String[] args){
-        EngineExpress engineExpress = new EngineExpress();
-        engineExpress.funcTest();
+//        EngineExpress engineExpress = new EngineExpress();
+//        engineExpress.funcTest();
+        logger.debug("EngineExpressTest logger.debug");
+    }
+
+    public int Func1(int a, int b)
+    {
+        return a+b;
     }
 }
---
+++ engineexpress-example/src/test/java/com/wuly/engineexpress/example/EngineExpressTestTest.java
@@ -0,0 +1,20 @@
+package com.wuly.engineexpress.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EngineExpressTestTest {
+    @Test
+    public void func1() throws Exception {
+        EngineExpressTest engineExpressTest = new EngineExpressTest();
+        assertEquals(3, engineExpressTest.Func1(1,2));
+    }
+
+    @Test
+    public void func2() throws Exception {
+        EngineExpressTest engineExpressTest = new EngineExpressTest();
+        assertEquals(3, engineExpressTest.Func1(1,2));
+    }
+
+}
